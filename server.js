7877a3f5cb7a7ef67c601be8098142ef596464f6
Your Name <business.joshuac@gmail.com>
2022-01-13 12:11:04 -0600

    Take one down pass it around

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const learnerRoute = require('./routes/learnerRoute')
 const greetingsRoute = require('./routes/greetingsRoute')
 const tipCalcRoute = require('./routes/tipCalcRoute')
 const magicRoute = require('./routes/magicRoute')
+const beerRoute = require('./routes/beerRoute')
 
 const app = express()
 const port = 4000
@@ -13,6 +14,7 @@ app.use('/learners', learnerRoute)
 app.use('/greetings', greetingsRoute)
 app.use('/tip', tipCalcRoute)
 app.use('/magic', magicRoute)
+app.use('/beer', beerRoute)
 
 app.get('/', (req, res)=>{
     res.status(200).json({message: "Api UP!"})
