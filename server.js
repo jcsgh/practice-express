6da2e26e45b4a04c76fc5063a69fbe96a7fe8920
Your Name <business.joshuac@gmail.com>
2022-01-13 10:02:09 -0600

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const app = require('express')
+
 const app = express()
 const port = 4000
+
+app.use(bodyParser.json())
 
 app.listen(port, ()=>{
     console.log("ONLINE")
