828d0aaaeb92a7edc6487c5f50f7228915522250
Your Name <business.joshuac@gmail.com>
2022-01-13 16:18:12 -0600

    form middleware

--- middlewares/idChecker.js
+++ middlewares/idChecker.js
@@ -8,11 +8,22 @@ const idCheck = (req, res, next) => {
     next()
 }
 
-// const isNumeric = (req, res, next) => {
-//     if(typeof id != "number") {
-//         res.status(400).json({message: 'SUPER NOPE'})
-//     }
-//     next()
+// FORM ->
+// {firstname: string min-length 2
+// lastname: string min-length 3
+// isCool: boolean
+// age: numbers min 18 max 28
 // }
 
-module.exports = idCheck
+const formCheck = (req, res, next) => {
+    const test_data = req.body
+    if(typeof test_data.firstname != "string" || typeof test_data.lastname != "string" || typeof test_data.password != "string" || typeof test_data.iscool != "boolean" || typeof test_data.age != "number") {
+        res.status(400).json({message: "Please enter correct data types"})
+    }
+    if (test_data.firstname.length < 2 || test_data.lastname.length < 3 || test_data.age.length < 18 || test_data.age.length > 28 ) {
+        res.status(400).json({message: "Values too long or too short"})
+    }
+    next()
+}
+
+module.exports = {idCheck, formCheck}
