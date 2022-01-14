89a926cb1a9c7dbf19baf6d385500de360aad2ef
Your Name <business.joshuac@gmail.com>
2022-01-14 11:32:08 -0600

    added card route and get

--- routes/cardRoute.js
+++ routes/cardRoute.js
@@ -1,9 +1,41 @@
 const express = require('express')
-const cardRouter = express.Router()
 const data = require('../data/cardData')
+const cardRouter = express.Router()
 
 cardRouter.get('/', (req, res)=>{
-    res.status(200).json({message: "API UP"})
+    res.status(200).json({cards: data})
+})
+
+cardRouter.get('/getvalue/:value', (req,res)=>{
+    let value = req.params.value
+    let arr = []
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].value === value) {
+            arr.push(data[i])
+        }
+    }
+    res.status(200).json({cards: arr})
+})
+
+cardRouter.get('/getcode/:code', (req,res)=>{
+    let code = req.params.code
+    let arr = []
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].code === code) {
+            arr.push(data[i])
+        }
+    }
+    res.status(200).json({cards: arr})
+})
+cardRouter.get('/getsuit/:suit', (req,res)=>{
+    let suit = req.params.suit
+    let arr = []
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].suit === suit.toUpperCase()) {
+            arr.push(data[i])
+        }
+    }
+    res.status(200).json({cards: arr})
 })
 
 module.exports = cardRouter
